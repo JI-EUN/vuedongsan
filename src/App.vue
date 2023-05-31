50b34843912df5fa80460d02b4269e749793f38d
JI-EUN <pangdoa_merry@daum.net>
2023-05-31 17:09:07 +0900

    컴포넌트 작업

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Modal :rommdata="rommdata" :clickNum="clickNum" :modalshow="modalshow"/>
+  <div class="menu">
+    <a v-for="(Links,i) in menu" :key="i" href="">{{ Links }}</a>
+  </div>
+
+  <Discount />
+
+
+  <div v-for="(lists, i) in rommdata" :key="i">
+    <img :src="lists.image" alt="">
+    <h4 @click="modal(i)">{{ lists.title }}</h4>
+    <p>{{ lists.price }}원</p>
+    <!-- <button @click="increase(i)">허위매물신고</button> <span>신고수 : {{ count[i] }}</span> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/data';
+import Discount from './Discount';
+import Modal from './Modal';
 
 export default {
   name: 'App',
+  //데이터 보관함 (중요 변수들)
+  data(){
+    return{
+      clickNum :0,
+      rommdata: data,
+      modalshow:false,
+      count:[0,0,0],
+      price1 : 80,
+      price2: 70,
+      menu:['Home','Shop','About'],
+      products:['역삼동 원룸', '천호동 원룸', '중구 원룸']
+    }
+  },  
+  methods:{
+    increase(i){
+      this.count[i]++
+    },
+    modal(i){
+      this.clickNum = i
+      if(this.modalshow){
+        this.modalshow = false;
+      }else{
+        this.modalshow = true;
+      }
+    }
+  },
   components: {
-    HelloWorld
+    Discount,
+    Modal
   }
 }
 </script>
@@ -21,6 +62,39 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+img{
+  max-width: 100%;
+}
+.discount{
+  background: #eee;
+  padding:10px;
+  margin:10px;;
+  border-radius : 5px;
+}
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
 }
 </style>
---
+++ src/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="black-bg" v-if="modalshow">
+        <div class="white-bg">
+            <h4>{{ rommdata[clickNum].title }}</h4>
+            <img :src="rommdata[clickNum].image" alt="">
+            <p>{{ rommdata[clickNum].price }}원</p>
+            <p>{{ rommdata[clickNum].content }}</p>
+
+            <!-- <button @click="modal()">모달 닫기</button> -->
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    props:{
+        rommdata: Object,
+        clickNum:Number,
+        modalshow:Boolean,
+    }
+}
+</script>
+
+<style>
+
+</style>
